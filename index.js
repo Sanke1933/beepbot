5d70bedfdd3cb661a15e53b6b847e3e04f76b048
megafreezer <36858947+megafreezer@users.noreply.github.com>
2019-02-02 18:04:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 })
 
-client.on('message', (message)=>{
-if(message.content == "b!play"){
-	message.member.voiceChannel.join().then(voice=>{
-       voice.playFile(__dirname+"/music.mp4");
-    const embed = new Discord.RichEmbed()
-	.setAuthor("BeepBot", "https://cdn.discordapp.com/attachments/358311606699950081/541241909201469470/1_7.PNG")
-        .setDescription("Я присоединился к голосовому каналу и включил музыку.")
-        .setColor("#42f4aa")
-message.channel.send({embed})
-})
-
 client.on('message', msg => {
 	if (msg.content === 'b!help') {
 	const embed = new Discord.RichEmbed()
