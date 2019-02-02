0923a108110fcb5f7ff7eef8f0c70b5cf69c327c
megafreezer <36858947+megafreezer@users.noreply.github.com>
2019-02-02 19:38:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let p = 'b!'
+let timen = d.toLocaleString();
 
 // 0 - Играть, 1 - Стримить, 2 - Слушать, 3 - Смотреть
 
@@ -18,6 +19,21 @@ client.on('message', message => {
 })
 
 client.on('message', msg => {
+	if (msg.content === 'b!si') {
+	   const embed = new Discord.RichEmbed()
+	   .setTitle(`Информация о сервере **${message.channel.guild.name}**`)
+	   .setAuthor(message.author.tag, message.author.avatarURL)
+	   .setThumbnail(message.channel.guild.iconURL)
+	   .setColor("#42f4aa")
+	   .addField('Вы присоединились к серверу', timen)
+	   .addField('Количество пользователей', message.channel.guild.member.count)
+	   .addField('Количество каналов', message.channel.guild.channels.size)
+	   .addField('Количество ролей',message.channel.guild.roles.size)
+	   .addField('Регион сервера', message.channel.guild.region)
+	   msg.channel.send({embed)}
+			    msg.react("✅")
+
+client.on('message', msg => {
 	if (msg.content === 'b!help') {
 	const embed = new Discord.RichEmbed()
 	.setAuthor("BeepBot", "https://cdn.discordapp.com/attachments/358311606699950081/541241909201469470/1_7.PNG")
@@ -26,5 +42,6 @@ client.on('message', msg => {
 	.setFooter(`${client.guilds.size} серверов`)
     .setColor("#42f4aa")
     msg.channel.send({embed});
+		msg.react("✅");
     }
 });
