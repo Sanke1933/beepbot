02550f162bcad5df79870728767674a78c7ecac8
megafreezer <36858947+megafreezer@users.noreply.github.com>
2019-02-02 20:39:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('message', message => {
 
 client.on('message', msg => {
 	if (msg.content === 'b!si') {
-		        if (msg.channel.guild.region == "russia") {
+		        if (msg.channel.guild.region === "russia") {
             msg.channel.guild.region = "Россия"
 		let d = msg.member.joinedAt; 
                 let timen = d.toLocaleString();
