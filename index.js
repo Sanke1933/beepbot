20d68a2d2a6417d00acc072deed3276e0e55b8a0
megafreezer <36858947+megafreezer@users.noreply.github.com>
2019-02-04 00:51:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,48 +17,48 @@
         const command = args.shift().toLowerCase();
     });
 
-    client.on('message', async msg => {
-        if (msg.content === `${p}serverinfo`) {
-            if (msg.channel.guild.region === "russia") {
-                msg.channel.guild.region = "Россия";
+    client.on('message', async message => {
+        if (message.content === `${p}serverinfo`) {
+            if (message.channel.guild.region === "russia") {
+                message.channel.guild.region = "Россия";
             }
-            let d = msg.member.joinedAt; 
+            let d = message.member.joinedAt; 
             let timen = d.toLocaleString();
             const embed = new Discord.RichEmbed()
-                .setTitle(`Информация о сервере ${msg.channel.guild.name}`)
-                .setAuthor(msg.author.tag, msg.author.avatarURL)
-                .setThumbnail(msg.channel.guild.iconURL)
+                .setTitle(`Информация о сервере ${message.channel.guild.name}`)
+                .setAuthor(message.author.tag, message.author.avatarURL)
+                .setThumbnail(message.channel.guild.iconURL)
                 .setColor("#e3caa0")
                 .addField('Вы присоединились к серверу', timen)
-                .addField('Количество пользователей', msg.channel.guild.memberCount)
-                .addField('Количество каналов', msg.channel.guild.channels.size)
-                .addField('Количество ролей',msg.channel.guild.roles.size)
-                .addField('Регион сервера', msg.channel.guild.region);
-            await msg.channel.send(embed);
-            msg.react("✅");
+                .addField('Количество пользователей', message.channel.guild.memberCount)
+                .addField('Количество каналов', message.channel.guild.channels.size)
+                .addField('Количество ролей',message.channel.guild.roles.size)
+                .addField('Регион сервера', message.channel.guild.region);
+            await message.channel.send(embed);
+            message.react("✅");
         }
 
-        if (msg.content.startsWith(`${p}avatar`)) {
-            let userok = msg.mentions.users.first();
+        if (message.content.startsWith(`${p}avatar`)) {
+            let userok = message.mentions.users.first();
             if (!userok)
-                userok = msg.author;    
+                userok = message.author;    
 
             const embed = new Discord.RichEmbed()
                 .setDescription(`Аватар пользователя ${userok}`)
                 .setImage(`${userok.avatarURL}`)
                 .setColor("#e3caa0");
-            await msg.channel.send(embed);
-            msg.react("✅");
+            await message.channel.send(embed);
+            message.react("✅");
         }
 
-        if (msg.content === `${p}help`) {
+        if (message.content === `${p}help`) {
             const embed = new Discord.RichEmbed()
                 .setAuthor("BeepBot", "https://cdn.discordapp.com/attachments/358311606699950081/541241909201469470/1_7.PNG")
                 .addField("Информация", "``b!serverinfo`` - информация о сервере.\n``b!avatar`` - украсть аватарку пользователя.")
                 .addField("Бот", "<@341988428457705482> (``@freezer. ⛧#4232``) - главный разработчик.(идея, бот, аватарка, код)\n<@421030089732653057> (``zziger#8040``) - второй разработчик.(код)")
                 .setFooter(`${client.guilds.size} серверов`)
                 .setColor("#e3caa0");
-            await msg.channel.send(embed);
-            msg.react("✅");
+            await message.channel.send(embed);
+            message.react("✅");
         };
     });
